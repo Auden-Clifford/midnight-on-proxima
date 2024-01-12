e5435f5801682db92110ba72793f770b872e28df
Auden-Clifford <112361970+Auden-Clifford@users.noreply.github.com>
2024-01-11 22:52:07 -0500

    added hitscan shooting

---
+++ Assets/Scripts/ShootHitscan.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootHitscan : MonoBehaviour
+{
+    [SerializeField] private Transform rayOriginPoint;
+    [SerializeField] private float force;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private LineRenderer lineRenderer;
+
+   public void Fire()
+   {
+        RaycastHit hit;
+
+        // cast a ray and chanc for a hit
+        if(Physics.Raycast(rayOriginPoint.position, rayOriginPoint.forward, out hit, maxDistance))
+        {
+            // check if threre is a rigidbody component
+            Rigidbody targetRigidbody = hit.collider.GetComponent<Rigidbody>();
+            if(targetRigidbody != null)
+            {
+                // add force to the hit object
+                targetRigidbody.AddForceAtPosition(rayOriginPoint.forward * force, hit.point, ForceMode.Impulse);
+            }
+        }
+
+        // draw the bullet trail
+        StartCoroutine(DrawShot(rayOriginPoint.position, hit.point));
+   }
+
+    private IEnumerator DrawShot(Vector3 start, Vector3 end)
+    {
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+        lineRenderer.enabled = true;
+
+        // wait a short time then hide again
+        yield return new WaitForSeconds(0.05f);
+
+        lineRenderer.enabled = false;
+    }
+
+}
